50ea1470e4f50a889d773318a44cdbe4a71dcebb
Moza USANE <mozamimy@quellencode.org>
2013-12-19 10:27:22 +0900

    class TransactionalMemory

--- transactional_memory.cpp
+++ transactional_memory.cpp
@@ -1,5 +1,15 @@
 #include "transactional_memory.h"
 
-TransactionalMemory::TransactionalMemory()
+TransactionalMemory::TransactionalMemory(int memorySize, int processCount)
 {
+    memory = new int[memorySize];
+    ownerships = new Rec*[memorySize];
+    recs = new Rec[processCount];
+}
+
+TransactionalMemory::~TransactionalMemory()
+{
+    delete[] memory;
+    delete[] ownerships;
+    delete[] recs;
 }
--- transactional_memory.h
+++ transactional_memory.h
@@ -1,10 +1,18 @@
 #ifndef TRANSACTIONAL_MEMORY_H
 #define TRANSACTIONAL_MEMORY_H
 
+#include "rec.h"
+
 class TransactionalMemory
 {
+private:
+    int*  memory;
+    Rec** ownerships;
+    Rec*  recs;
+
 public:
-    TransactionalMemory();
+    TransactionalMemory(int memorySize, int processCount);
+    ~TransactionalMemory();
 };
 
 #endif // TRANSACTIONAL_MEMORY_H
